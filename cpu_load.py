2efbbb7a7c188aadf3befbd1834825e2db80719a
Tom Churchill <tom@tomchurchill.co.uk>
2023-12-30 19:28:24 +0000

    v2

--- cpu_load.py
+++ cpu_load.py
@@ -8,7 +8,7 @@ def cpu_load():
     start_time = time.time()
     while time.time() - start_time < 1:
         pass
-    return f'{start_time}\n'
+    return f'2 {start_time}\n'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
