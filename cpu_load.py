351ebbd58ba8027cf4d49fc090f54304477f8569
Tom Churchill <tom@tomchurchill.co.uk>
2024-01-12 15:11:12 +0000

    v3

--- cpu_load.py
+++ cpu_load.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import time
 
 app = Flask(__name__)
@@ -6,9 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def cpu_load():
     start_time = time.time()
-    while time.time() - start_time < 1:
+    count = int(request.args.get('count', 10))
+    for i in range(count):
         pass
-    return f'2 {start_time}\n'
+    return f'3 {time.time() - start_time}\n'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
